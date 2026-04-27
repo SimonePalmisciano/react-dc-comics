c52bdeb60564fca616ec5e55bca6aca20746ee9d
Simone Palmisciano <simonepalmisciano11@gmail.com>
2026-04-27 18:25:11 +0200

    stile footer sezione merch e sezione navlinks

--- src/components/FooterListaMerch.jsx
+++ src/components/FooterListaMerch.jsx
@@ -9,24 +9,32 @@ function FooterListaMerch() {
                         </div>
                         <p>digital comics</p>
                     </a>
+                </li>
+                <li>
                     <a href="#">
                         <div>
                             <img src="/img/comics-merchandise.png" alt="" />
                         </div>
                         <p>dc merchandise</p>
                     </a>
+                </li>
+                <li>
                     <a href="#">
                         <div>
                             <img src="/img/subscriptions.png" alt="" />
                         </div>
                         <p>subscription</p>
                     </a>
+                </li>
+                <li>
                     <a href="#">
                         <div>
                             <img src="/img/comics-shop.png" alt="" />
                         </div>
                         <p>comic shop locator</p>
                     </a>
+                </li>
+                <li>
                     <a href="#">
                         <div>
                             <img src="/img/dc-power-visa.svg" alt="" />
