4c935e2e21f856b1a3c8feae3064a2249b0bbb64
Simone Palmisciano <simonepalmisciano11@gmail.com>
2026-04-27 18:49:37 +0200

    aggiunto logo come bg nel footer

--- src/components/FooterSectionLinks.jsx
+++ src/components/FooterSectionLinks.jsx
@@ -3,6 +3,7 @@ import FooterNavlinks from "./FooterNavlinks";
 function FooterSectionLinks() {
     return (
         <section className="section-navlinks">
+            {/* <img src="/img/dc-logo-bg.png" alt="" /> */}
             <div className="container">
                 <FooterNavlinks />
             </div>
