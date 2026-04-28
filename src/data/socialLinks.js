6c8751714e55dcbad85ce8c2dab1c454c53219f6
Simone Palmisciano <simonepalmisciano11@gmail.com>
2026-04-28 15:31:15 +0200

    creato data links social

---
+++ src/data/socialLinks.js
@@ -0,0 +1,34 @@
+const socialLinks = [
+    {
+        id: 1,
+        src: '/img/footer-facebook.png',
+        name: 'facebook',
+        link: '#'
+    },
+    {
+        id: 2,
+        src: '/img/footer-twitter.png',
+        name: 'twitter',
+        link: '#'
+    },
+    {
+        id: 3,
+        src: '/img/footer-youtube.png',
+        name: 'youtube',
+        link: '#'
+    },
+    {
+        id: 4,
+        src: '/img/footer-pinterest.png',
+        name: 'pinterest',
+        link: '#'
+    },
+    {
+        id: 1,
+        src: '/img/footer-periscope.png',
+        name: 'periscope',
+        link: '#'
+    },
+];
+
+export {socialLinks};
