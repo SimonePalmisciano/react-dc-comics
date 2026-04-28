2346a5b297b48487967e8af495689f0c4ef3cfa6
Simone Palmisciano <simonepalmisciano11@gmail.com>
2026-04-28 15:21:26 +0200

    creato data links, footer

---
+++ src/data/navlinks.js
@@ -0,0 +1,118 @@
+const dcComicsLinks = [
+    {
+        id: 1,
+        text: 'Characters'
+    },
+    {
+        id: 2,
+        text: 'Comics'
+    },
+    {
+        id: 3,
+        text: 'Movies'
+    },
+    {
+        id: 4,
+        text: 'TV'
+    },
+    {
+        id: 5,
+        text: 'Games'
+    },
+    {
+        id: 6,
+        text: 'Videos'
+    },
+    {
+        id: 7,
+        text: 'News'
+    },
+];
+
+const shopLinks = [
+    {
+        id: 1,
+        text: 'Shop DC'
+    },
+    {
+        id: 2,
+        text: 'Shop DC Collectibles'
+    },
+];
+
+const dcLinks = [
+    {
+        id: 1,
+        text: 'Terms Of Use'
+    },
+    {
+        id: 2,
+        text: 'Privacy policy'
+    },
+    {
+        id: 3,
+        text: 'Ad Choice'
+    },
+    {
+        id: 4,
+        text: 'Advertising'
+    },
+    {
+        id: 5,
+        text: 'Jobs'
+    },
+    {
+        id: 6,
+        text: 'Subscriptions'
+    },
+    {
+        id: 7,
+        text: 'Talent Workshop'
+    },
+    {
+        id: 8,
+        text: 'CPSC Certificates'
+    },
+    {
+        id: 9,
+        text: 'Ratings'
+    },
+    {
+        id: 10,
+        text: 'Shop Help'
+    },
+    {
+        id: 11,
+        text: 'Contact us'
+    },
+];
+
+const sitesLinks = [
+    {
+        id: 1,
+        text: 'DC'
+    },
+    {
+        id: 2,
+        text: 'MAD Magazine'
+    },
+    {
+        id: 3,
+        text: 'DC Kids'
+    },
+    {
+        id: 4,
+        text: 'DC Universe'
+    },
+    {
+        id: 5,
+        text: 'DC Power Visa'
+    },
+];
+
+export {
+    dcComicsLinks,
+    shopLinks,
+    dcLinks,
+    sitesLinks
+};
