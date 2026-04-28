27aecc304b5d0624dfbac5e4a6b711a853ee9826
Simone Palmisciano <simonepalmisciano11@gmail.com>
2026-04-28 16:23:02 +0200

    create piccole card dei fumetti

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,32 @@
+import comics from "../data/comics.js";
+
+
+const fumettiJsx = comics.map(fumetto => {
+    const { id, title, description, thumb, price, series } = fumetto;
+    return (
+        <li key={id}>
+            <div className="img-fumetti-container">
+                <img src={ thumb } alt={ title } />
+            </div>
+            <p>{ series }</p>
+        </li>
+    );
+})
+
 function Main() {
     return <>
         <main>
-            <div className="container">
-                <h1>-- Content goes here --</h1>
-            </div>
+            <section className="all-bg">
+            </section>
+            <section>
+                <div className="container">
+                    <div className="container-comics">
+                        <ul>
+                            { fumettiJsx }
+                        </ul>
+                    </div>
+                </div>
+            </section>
         </main>
     </>;
 }
